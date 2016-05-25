db324c23183c35c6e3feb445cdce9badeb53b725
Whan, Alex (PI, Black Mountain) <Alex.Whan@csiro.au>
2016-05-25 17:07:04 +1000

    added tests for isHet/Homo

---
+++ tests/testthat/test-scoreConverters.R
@@ -0,0 +1,17 @@
+context("converting genotyping scores")
+
+test_that("isHet works", {
+  expect_false(isHet("AA"))
+  expect_true(isHet("AG"))
+  expect_error(isHet('u'))
+  expect_error(isHet("GT", "[AB]", "error"))
+  expect_error(isHet("GT", "[AB]", "other"))
+})
+
+test_that("isHomo works", {
+  expect_false(isHomo("AG"))
+  expect_true(isHomo("AA"))
+  expect_error(isHomo('u'))
+  expect_error(isHomo("TT", "[AB]", "error"))
+  expect_error(isHomo("GT", "[AB]", "other"))
+})
