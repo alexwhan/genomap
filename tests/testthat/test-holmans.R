ba9e2cdb5804f5895603de44df19466307347ab7
Whan, Alex (PI, Black Mountain) <Alex.Whan@csiro.au>
2016-06-01 13:41:28 +1000

    fixed typos in tests

--- tests/testthat/test-holmans.R
+++ tests/testthat/test-holmans.R
@@ -1,15 +1,15 @@
 context("Testing holmans functions")
 
-load("tests/test_data/holmans_data.rda")
-test_that("trixy converts accurately") {
+load("../test_data/holmans_data.rda")
+test_that("trixy converts accurately", {
   expect_error(trixy(mtcars))
   expect_error(trixy(mean))
   expect_warning(trixy(tri_df * 3))
   expect_identical(genomap:::trixy(tri_df), tri_trixy_df)
-}
+})
 
-test_that("ggholmans producing plots correctly") {
+test_that("ggholmans producing plots correctly", {
   expect_error(ggholmans(mtcars))
   expect_warning(ggholmans(tri_df * 3))
   expect_equal(ggholmans(tri_df), holmans_tri)
-}
+})
