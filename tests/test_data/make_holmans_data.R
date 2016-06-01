73effa1b81e105b1b076d408265ccf4236253623
Whan, Alex (PI, Black Mountain) <Alex.Whan@csiro.au>
2016-06-01 13:10:40 +1000

    modified test data for holmans - added converted df and plot output

--- tests/test_data/make_holmans_data.R
+++ tests/test_data/make_holmans_data.R
@@ -6,6 +6,8 @@ tri_df <- data_frame(AA = abs(rnorm(10, mean = 0, sd = 0.15)),
   rowwise() %>%
   mutate(AB = 1 - sum(AA, BB))
 
+tri_trixy_df <- genomap:::trixy(tri_df)
+
 holmans_tri <- genomap::ggholmans(tri_df)
-devtools::use_data(tri_df)
-devtools::use_data(holmans_tri, internal = TRUE)
+
+save(tri_df, tri_trixy_df, holmans_tri, file = "tests/test_data/holmans_data.rda")
