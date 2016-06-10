a9a2540e6407930dfbeb363c4cb9227b0e10072b
Whan, Alex (PI, Black Mountain) <Alex.Whan@csiro.au>
2016-06-10 11:57:54 +1000

    added data for check_parents_ tests

--- tests/test_data/make_genotype_data.R
+++ tests/test_data/make_genotype_data.R
@@ -9,10 +9,19 @@ genotype_raw_df <- data_frame(markerName = paste0("M", 1:6),
                            prog3 = c("GG", "CC", "GG", "CC", "AG", "TC"),
                            prog4 = c("AG", "TT", "AG", "CC", "AG", "TC"))
 
+saveRDS(genotype_raw_df, file = "tests/test_data/genotype_raw_df.rds")
+
 genotype_rel_df <- data_frame(markerName = paste0("M", 1:3),
                               prog1 = c("AA", "AA", "BB"),
                               prog2 = c("AA", "BB", "BB"),
                               prog3 = c("BB", "BB", "AA"),
                               prog4 = c("AB", "AA", "AB"))
 
-devtools::use_data(genotype_raw_df, genotype_rel_df)
+saveRDS(genotype_rel_df, file = "tests/test_data/genotype_raw_df.rds")
+
+genotype_parent_status_df <- genotype_raw_df %>%
+  mutate(parent_status = c("informative", "informative", "informative",
+                            "nonseg", "heterozygous", "heterozygous")) %>%
+  select(parent1, parent2, parent_status, markerName, contains("prog"))
+
+saveRDS(genotype_parent_status_df, file = "tests/test_data/genotype_parent_status_df.rds")
