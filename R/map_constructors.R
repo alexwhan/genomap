ea0aac10a928bda249d098194c419d0f58f51441
Whan, Alex (PI, Black Mountain) <Alex.Whan@csiro.au>
2016-07-08 14:34:31 +1000

    added get_lg_stats function

--- R/map_constructors.R
+++ R/map_constructors.R
@@ -12,3 +12,19 @@ asmap_step <- function(obj, p.value = 1e-6) {
   obj$p.value <- p.value
   return(obj)
 }
+
+#' Returns the maximum distance between markers for a
+#' linkage group.
+#'
+#' @obj A cross object suitable for mstmap().
+get_lg_stats <- function(obj) {
+  lg_stats <- lapply(obj$geno, function(x) {
+    if(length(x$map) > 1){
+      diff_ord <- diff(x$map)[order(diff(x$map))]
+      max_diff <- diff_ord[length(diff_ord)]
+      return(max_diff = setNames(max_diff, ""))
+    } else {
+      return()
+    }
+  }) %>% unlist()
+}
