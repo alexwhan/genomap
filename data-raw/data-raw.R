4ee0bcab18a1456ffb07710732c90549f6017b90
Alex Whan <alexwhan@gmail.com>
2017-02-24 00:16:13 +1100

    added an 8way mpcross and a 4way mpInterval object

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -1,5 +1,6 @@
 library(qtl)
 library(mpMap)
+library(mpwgaim)
 
 set.seed(1234)
 bp_map <- sim.map(len = sample(50:150, 6), n.mar = sample(30:70, 6), 
@@ -14,3 +15,5 @@ m4_cross <- sim.mpcross(bp_map, m4_ped)
 m8_ped <- sim.mpped(8, 7, 1, 6, 6)
 
 m8_cross <- sim.mpcross(bp_map, m8_ped)
+
+m4_int <- mpcross2int(m4_cross)
